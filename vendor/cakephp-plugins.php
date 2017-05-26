b80be2d9f8a95277189da603ae52f59944179fb0
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2017-05-26 20:55:16 -0300

    Versão 1.12

--- vendor/cakephp-plugins.php
+++
@@ -1,8 +0,0 @@
-<?php
-$baseDir = dirname(dirname(__FILE__));
-return [
-    'plugins' => [
-        'Bake' => $baseDir . '/vendor/cakephp/bake/',
-        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
-    ]
-];
